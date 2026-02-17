09b908f961ba25585a4e952860b00c569cabe642
naveenkumar-sys <naveenrnk69@gmail.com>
2026-02-17 16:03:49 +0530

    project section updated

--- src/data/projects.js
+++ src/data/projects.js
@@ -3,24 +3,25 @@ import ecom from "/images/project/eccomerce.jpg";
 import movie from "/images/project/movieReview.png";
 import recipe from "/images/project/RecipeHub.webp";
 import zion from "/images/project/zion.png";
+import sd from "/images/project/physiocare.png"
 
 const projects = [
   {
     title: "Movie Review",
     image: movie,
-    link: "https://github.com/naveenkumar-sys/movie-Review",
+    link: "https://movie-review-453f.vercel.app/",
     points: ["React", "API Fetch", "Responsive UI"],
   },
   {
     title: "Recipe Hub",
     image: recipe,
-    link: "https://github.com/naveenkumar-sys/Recipe-Hub",
+    link: "https://recipe-hub-lime.vercel.app/",
     points: ["Search recipes", "Dynamic UI", "Clean layout"],
   },
   {
     title: "MAAZA 3D Website",
     image: maaza,
-    link: "https://github.com/naveenkumar-sys/maaza",
+    link: "https://maaza.vercel.app/",
     points: ["Three.js", "Scroll animation", "3D UI"],
   },
   {
@@ -38,6 +39,16 @@ const projects = [
     role: "Frontend Development",
     github: "https://github.com/...",
     live: "https://ziongroups.com",
+  },
+   {
+    title: "physiotherapy website",
+    description:
+      "website built with modern UI, responsive layout, and optimized performance for production hosting.",
+    image: sd,
+    tech: ["React", "Tailwind", "Framer Motion"],
+    role: "Frontend Development",
+    github: "https://github.com/...",
+    live: "https://physiocare-p5wf.vercel.app/",
   },
 ];
 
